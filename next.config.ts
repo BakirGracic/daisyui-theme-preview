4716f1f9fba3de0c2bbfd7c6d5c7e22e7647215a
BakirGracic <bakirgdev@gmail.com>
2024-10-27 20:53:56 +0100

    try to fix gh pages build & deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "https://bakirgracic.github.io/daisyui-theme-preview",
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
